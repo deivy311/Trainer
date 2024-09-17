e88e23840f816e66f7c7b62c9a125cc4da7c906e
David Esteban Imbajoa Ruiz <deivy311@hotmail.com>
2024-09-17 13:22:42 +0400

    Updated pointer example

--- Section02/Pointers/Pointers/Pointers/Pointers.cpp
+++ Section02/Pointers/Pointers/Pointers/Pointers.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
+void testReference( int &a)
+{
+	a++;
+}
 int main() {
 	using namespace std;
 	int x = 10;
 	cout << &x << "\n";
-	int *ptr = nullptr;
+	int *ptr = &x;
 	//cout << *ptr << "\n";
+	testReference(x);
+
 	*ptr = 10 ;
 	return 0; 
 }
