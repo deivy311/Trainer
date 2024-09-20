44e0273709c28bef98c946e9fa35cdc8aa270175
salama.matar <salama.matar@adasi.ae>
2024-09-20 14:08:19 +0400

    add some lines of code in section 5 tutorial

--- Section05/Lvaluesandrvalues/Rvalues/Rvalues01/Rvalues01.cpp
+++ Section05/Lvaluesandrvalues/Rvalues/Rvalues01/Rvalues01.cpp
@@ -17,28 +17,47 @@ void Print(const int &x) {
 	std::cout << "Print(const int&)" << std::endl;
 
 }
+
+//accepet r-value reference
 void Print(int &&x) {
 	std::cout << "Print(int &&)" << std::endl;
 }
 int main() {
-	//x is lvalue
-	int x = 10;
+	// //x is lvalue
+	// int x = 10;
 	
-	//ref is l-value reference
-	int &ref = x ;
-	//Transform returns an l-value
-	int &ref2 = Transform(x) ;
-	//Binds to function that accepts l-value reference
-	Print(x);
+	// //ref is l-value reference
+	// int &ref = x ;
+	// //Transform returns an l-value
+	// int &ref2 = Transform(x) ;
+	// //Binds to function that accepts l-value reference
+	// Print(x);
 	
 
-	//rv is r-value reference
-	int &&rv = 8 ;
+	// //rv is r-value reference
+	// int &&rv = 8 ;
 	
-	//Add returns a temporary (r-value)
-	int &&rv2 = Add(3,5) ;
-	//Binds to function that accepts a temporary, i.e. r-value reference
-	Print(3);
-	return 0;
+	// //Add returns a temporary (r-value)
+	// int &&rv2 = Add(3,5) ;
+	// //Binds to function that accepts a temporary, i.e. r-value reference
+	// Print(3);
+	// return 0;
+
+
+	// my practice below ------------------------
+
+   int &&r1 = 10;
+   int &&r2 = Add(3,2);
+   int x =0; //l value 
+   Print(x); // call l value function
+   Print(3);
+   //int &&r3 = x;//connot assign l value to a r value
+
+   int &ref = Transform(x);
+   const int &ref2 = 3;
+
+   
+   return 0;
+
 }
 
